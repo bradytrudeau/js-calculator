cb3f4ab8061e1b07d87b1b31967eadaff2fc3f0a
Brady Trudeau <bradytrudeau@gmail.com>
2021-03-03 21:32:28 -0600

    Revert back to local hosting

--- server/pool.js
+++ server/pool.js
@@ -1,14 +1,22 @@
-const { Pool } = require('pg');
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
+const pg = require('pg');
+const Pool = pg.Pool;
+require('dotenv').config();
 
-pool.on('error', (err) => {
-  console.log('Unexpected error on idle client', err);
-  process.exit(-1);
-});
+config = {
+  host: 'localhost',
+  port: process.env.PGPORT, 
+  database: process.env.PGDATABASE, 
+  max: 10,
+  idleTimeoutMillis: 30000, 
+};
 
+const pool = new Pool(config);
+
+pool.on('connect', (client) => {
+    console.log('pg connected');
+})
+pool.on('error', (err, client) => {
+    console.log('Unexpected error on idle pg client', err);
+    process.exit(-1);
+});
 module.exports = pool;
