3ad486a5a4323307efa81104c6f6a955585cd97e
Brady Trudeau <bradytrudeau@gmail.com>
2021-03-03 21:16:34 -0600

    Heroku tweaks

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,7 +1,11 @@
-const proxy = require("http-proxy-middleware");
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
 module.exports = function(app) {
-  app.use(proxy("/api/**", { // https://github.com/chimurai/http-proxy-middleware
-    target: "http://localhost:5000",
-    secure: false
-  }));
+  app.use(
+    '/',
+    createProxyMiddleware({
+       target: 'http://localhost:6000',
+       changeOrigin: true
+    })
+  );
 };
