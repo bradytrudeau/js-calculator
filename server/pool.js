578081e41fd8c10a968d6f333c55e2c50d71c832
Brady Trudeau <bradytrudeau@gmail.com>
2021-03-03 19:39:50 -0600

    Adjusting Pool

--- server/pool.js
+++ server/pool.js
@@ -3,7 +3,6 @@ const Pool = pg.Pool;
 require('dotenv').config();
 const url = require('url');
 
-let config = {};
 
 if (process.env.DATABASE_URL) {
   const params = url.parse(process.env.DATABASE_URL);
@@ -19,15 +18,41 @@ if (process.env.DATABASE_URL) {
     max: 10, 
     idleTimeoutMillis: 30000,
   };
-} else {
-  config = {
-    host: 'localhost',
-    port: process.env.PGPORT, 
-    database: process.env.PGDATABASE, 
-    max: 10,
-    idleTimeoutMillis: 30000, 
-  };
-}
+
+  
+// config = {
+//   host: 'localhost',
+//   port: process.env.PGPORT, 
+//   database: process.env.PGDATABASE, 
+//   max: 10,
+//   idleTimeoutMillis: 30000, 
+// };
+
+// let config = {};
+
+// if (process.env.DATABASE_URL) {
+//   const params = url.parse(process.env.DATABASE_URL);
+//   const auth = params.auth ? params.auth.split(':') : [null, null];
+
+//   config = {
+//     user: auth[0],
+//     password: auth[1],
+//     host: params.hostname,
+//     port: params.port,
+//     database: params.pathname.split('/')[1],
+//     ssl: { rejectUnauthorized: false },
+//     max: 10, 
+//     idleTimeoutMillis: 30000,
+//   };
+// } else {
+//   config = {
+//     host: 'localhost',
+//     port: process.env.PGPORT, 
+//     database: process.env.PGDATABASE, 
+//     max: 10,
+//     idleTimeoutMillis: 30000, 
+//   };
+// }
 
 const pool = new Pool(config);
 
